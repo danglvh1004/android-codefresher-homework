cb2d38fb72800c4e6cb53c072274211e49c9c212
danglvh1004 <dang.lvh.1004@gmail.com>
2021-07-09 11:34:11 +0700

    Commit Lesson10

---
+++ Lesson10/app/src/main/java/com/example/lesson10/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.lesson10;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private AlbumListAdapter adapter;
+    private List<Album> albumList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUI();
+
+        albumList = new ArrayList<>();
+
+        adapter = new AlbumListAdapter(this);
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        callApi();
+    }
+
+    private void initUI() {
+        recyclerView = findViewById(R.id.recyclerview);
+    }
+
+    private void callApi() {
+        ApiService.apiService.getAllAlbums().enqueue(new Callback<List<Album>>() {
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                albumList = response.body();
+
+                if (albumList != null) {
+                    adapter.setData(albumList);
+                    displayToast("Call Api Sucessfully");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable t) {
+                displayToast("Call Api Fail");
+            }
+        });
+    }
+
+    private void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ Lessson10/app/src/main/java/com/example/lessson10/AlbumListAdapter.java
@@ -0,0 +1,78 @@
+package com.example.lessson10;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class AlbumListAdapter extends RecyclerView.Adapter<AlbumListAdapter.AlbumListViewHolder> {
+
+    private List<Album> albumList;
+    private Context context;
+
+    public AlbumListAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(List<Album> list) {
+        this.albumList = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public AlbumListViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.album_layout, parent, false);
+        return new AlbumListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull AlbumListAdapter.AlbumListViewHolder holder, int position) {
+        Album album = albumList.get(position);
+
+        if (album == null) {
+            return;
+        } else {
+            String strId = String.valueOf(album.getId());
+            holder.tvId.setText(strId);
+            holder.tvTitle.setText(album.getTitle());
+            Picasso.get().load(album.getUrl()).fit().into(holder.imgAlbum);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (albumList != null) {
+            return albumList.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public class AlbumListViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvId;
+        private TextView tvTitle;
+        private ImageView imgAlbum;
+
+        public AlbumListViewHolder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
+            super(itemView);
+
+            imgAlbum = itemView.findViewById(R.id.img_album);
+            tvId = itemView.findViewById(R.id.tv_id);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+        }
+    }
+}
